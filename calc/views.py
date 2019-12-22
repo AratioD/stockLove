c004f95d4d6676efde50bbb56b0a806e297b3181
AratioD <aaa@sss.com>
2019-12-22 17:55:36 +0200

    not working correctly yet, assuming that it will

--- calc/views.py
+++ calc/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{'name':'Jack'});
+    return render(request, 'home.html',{'name':'Jack'})
+
+
+def add(request):
+    
+    val1 = request.GET['num1']
+    val2 = request.GET['num2']
+    
+    res = val1 + val2
+    return render(request, 'result.html',{'result':res})
