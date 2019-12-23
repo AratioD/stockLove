e8fb9f8c56bbc6e4c24fb1adfcee3291fea3c5db
AratioD <aaa@sss.com>
2019-12-23 20:50:46 +0200

    now travello site working, but not looking nice

--- travello/urls.py
+++ travello/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('add', views.add, name='add')
+    path('', views.index, name='home')
+    # path('add', views.add, name='add')
 ]
